2d8934dfc67ce6fdbe1bac5c6a32c1ba578de7ca
zaheenrakib <rakibislambest@gmail.com>
2024-11-07 11:40:10 +0600

    Some

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -20,6 +20,18 @@ const CustomNavbar = () => {
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       {/* Your nav items here */}
       <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+        <a href="#" className="flex items-center">Home</a>
+      </Typography>
+      <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+        <a href="#" className="flex items-center">Blogs</a>
+      </Typography>
+      <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+        <a href="#" className="flex items-center">Pages</a>
+      </Typography>
+      <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+        <a href="#" className="flex items-center">Pages</a>
+      </Typography>
+      <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
         <a href="#" className="flex items-center">Pages</a>
       </Typography>
       {/* Add more nav items as needed */}
